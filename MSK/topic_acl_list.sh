1fdbf6f7945d1a10ecfc3cfbacd7147175ed635e
stksinha <stksinha@gmail.com>
2026-05-10 18:24:02 +0100

    Fix path to kafka-acls.sh in topic_acl_list.sh

--- MSK/topic_acl_list.sh
+++ MSK/topic_acl_list.sh
@@ -19,7 +19,7 @@ echo "Listing acls of '$TOPIC' on broker '$BROKER'..."
 export KAFKA_HEAP_OPTS="-Xmx1G -Xms512M"
 
 #Listing acls for topics
-/opt/kafka/kafka/bin/kafka-acls.sh \
+$KAFKA_PATH/bin/kafka-acls.sh \
 --bootstrap-server "$BROKER" \
 --list \
 --topic "$TOPIC" \
