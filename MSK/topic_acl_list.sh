1d6c0f44756e39109730d63c0c65a99c1817e052
stksinha <stksinha@gmail.com>
2026-05-10 17:38:00 +0100

    Add topic_acl_list.sh to list Kafka topic ACLs
    Script lists ACLs for a specified Kafka topic.

---
+++ MSK/topic_acl_list.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Usage: ./topic_acl_list.sh <BROKER_HOST:PORT> <TOPIC_NAME>
+# Example: ./topic_list.sh localhost:9092 my-topic
+
+BROKER=$1
+TOPIC=$2
+
+if [ -z "$BROKER" ] || [ -z "$TOPIC" ]; then
+   echo "Usage:$0 <BROKER_HOST:PORT><TOPIC_NAME>"
+   exit 1
+fi
+
+echo "Listing acls of '$TOPIC' on broker 'BROKER'..."
+export KAFKA_HEAP_OPTS="-Xmx1G -Xms512M"
+
+#Listing acls for topics
+/opt/kafka/kafka/bin/kafka-acls.sh \
+--bootstrap-server "$BROKER" \
+--list \
+--topic "$TOPIC" \
+--command-config /opt/kafka/kafka/config/client.properties
